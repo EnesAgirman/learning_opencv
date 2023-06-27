a7718228acf665a446d47b076abb6345c732c201
Enes Ağırman <99555923+EnesAgirman@users.noreply.github.com>
2023-06-27 09:33:02 +0200

    read_and_display_image.py

---
+++ read_and_display_image.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+
+# READING AND DISPLAYING AN IMAGE
+
+Blue_S2000 = cv.imread('random_images\Honda_S2000_Blue.jpg')    # reads the image and stores it as a matrix
+
+cv.imshow("CAR", Blue_S2000)    # display the image
+
+cv.waitKey(10000)    # waits 3000 miliseconds for you to press any key, closes the window even if you haven't pressed after 3000 miliseconds
+
